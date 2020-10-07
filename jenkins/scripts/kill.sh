8f5ca0d0342a1a23322fbfc775244aeee8de1dad
bhaskarrebba <rebba.bhaskararao@gmail.com>
2020-10-07 09:06:18 +0530

    changes added

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
+
+echo 'The following command terminates the "npm start" process using its PID'
+echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
+echo 'was executed.'
 set -x
 kill $(cat .pidfile)
-npm start &
 npm run node-server &
